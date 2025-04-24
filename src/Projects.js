f2a475f4c83108bcb29782d640447963e47e8604
Hemant <hemantdevgan.0098@gmail.com>
2025-04-25 01:25:26 +0530

    vote_app link added

--- src/Projects.js
+++ src/Projects.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Projects.css';
+import { href } from 'react-router-dom';
 
 function Projects() {
   return (
     <section className="projects" id="projects">
       <h2 className="section-title">My Projects</h2>
       <div className="project-grid">
-        <div className="project-card">
+        <div className="project-card" onClick={"window.location.href='https://hemant0098.github.io/vote_app'"}>
           <h3>Voting App</h3>
           <p>A secure online voting platform.</p>
         </div>
